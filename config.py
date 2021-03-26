83d3cc544dc52288778486d4c42bba8b612575b4
frosty-nee <frostbite@frosty-nee.net>
2021-03-26 00:00:21 +0000

    forgot to fix write to actually work

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from yaml import dump
 from yaml import safe_load as load
 
 
-
 def read():
     with open('config.yaml', 'r') as f:
         return load(f)
@@ -10,14 +9,17 @@ def read():
 
 def write():
     with open('config.yaml', 'w') as f:
-        dump(config, f)
+        dump({
+            'app_secret_key': app_secret_key,
+            'esi_client_id': esi_client_id,
+            'esi_secret_key': esi_secret_key,
+            'testing': testing,
+            }, f)
+
 
 def update_from_file():
     for k,v in read().items():
         globals()[k] = v
 
+
 update_from_file()
-
-
-if __name__ == '__main__':
-    pass
